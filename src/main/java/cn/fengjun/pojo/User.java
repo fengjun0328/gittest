d7770e41082ca4a6cb89673ecac2d5d7cf5c1a69
fengjun0328 <783585655@qq.com>
2019-01-18 14:25:21 +0800

    搭环境

--- src/main/java/cn/fengjun/pojo/User.java
+++ src/main/java/cn/fengjun/pojo/User.java
@@ -1,4 +1,13 @@
 package cn.fengjun.pojo;
 
 public class User {
+    private String userName;
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
 }
